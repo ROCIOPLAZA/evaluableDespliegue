afd39d3d569f250798df364c17cdd6fbddbd911d
Rocio Plaza <rocioplazagar@gmail.com>
2024-04-21 11:36:09 +0200

    Commit subiendo ficheros para el evaluable de despliegue

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+
+    public static void bucle() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hola esto es un código de prueba para el evaluable!!");
+
+        bucle();
+    }
+}
