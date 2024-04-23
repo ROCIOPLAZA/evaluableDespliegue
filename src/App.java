c5062d78e2d4442fa4302a9efe4df1055442b309
Rocio Plaza <rocioplazagar@gmail.com>
2024-04-23 15:02:48 +0200

    Subiendo cambios para el reto

--- src/App.java
+++ src/App.java
@@ -4,11 +4,15 @@ public class App {
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
+
+        System.out.println(
+                "Acabo de realizar un bucle del 1 al 10 para que salga cada número una debajo del otro. Y esto añadiendo este código para el cambio del reto");
     }
 
     public static void main(String[] args) throws Exception {
         System.out.println("Hola esto es un código de prueba para el evaluable!!");
 
         bucle();
+
     }
 }
